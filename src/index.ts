125f4591d3300e01dee17bce0dd77d5cc9309917
felipebat20 <felipebatista24680@gmail.com>
2023-09-27 22:56:35 -0300

    add: cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
+import cors from 'cors';
 import { createHttpTerminator } from 'http-terminator';
 
 import routes from './routes';
@@ -8,6 +9,8 @@ import routes from './routes';
 dotenv.config();
 
 const app: Application = express();
+
+app.use(cors());
 const port = process.env.APP_PORT || 8000;
 export const server = http.createServer(app);
 export const httpTerminator = createHttpTerminator({
